574feaf28b110e3387e53a910f1d21284c3c5f20
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2020-09-12 09:25:55 +0200

    Update test.js

--- test.js
+++ test.js
@@ -4,8 +4,7 @@ const config = require("./config.json")
 
 async function init() {
 	const token = await PayPalInvoices.getToken(config.clientID, config.secretID);
-	//PayPalInvoices.createInvoice(token, "test@thatsfordevelopment.at", "test", "5", "1")
+	PayPalInvoices.createInvoice(token, "test@thatsfordevelopment.at", "test", "5", "1")
 	PayPalInvoices.createInvoiceQRCode(token, "INV2-SQWP-6UB6-8JEM-EXF9")
 }
-//https: //www.sandbox.paypal.com/invoice/p/#INV2UDXGSFY2Q46RAVXL
 init();
