ecb898986ccf071cc9493be29f02000665d8656b
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2020-09-12 19:58:57 +0200

    Update createInvoice.js

--- examples/createInvoice.js
+++ examples/createInvoice.js
@@ -5,13 +5,6 @@ const config = require("../config/config.json")
 async function init() {
 
 	const token = await PayPalInvoices.getToken(config.clientID, config.secretID);
-	// -> returns token
-
 	PayPalInvoices.createInvoice(token, "test@thatsfordevelopment.at", "test", "5", "1")
-	// -> {
-	// ->  rel: 'self',
-	// ->  href: 'https://api.sandbox.paypal.com/v2/invoicing/invoices/INV2-W8VG-4SY5-BTJ2-6HM6',
-	// ->  method: 'GET'
-	// -> }
 }
 init();
