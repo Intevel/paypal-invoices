ff3ba2a4a61eedc128157cc68a826e9fb94bf680
Intevel ツ <59223342+Intevel@users.noreply.github.com>
2020-09-13 08:07:00 +0200

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const config = require("./config/config.json");
-const PayPalInvoices = require("./src");
-
-async function init() {
-	const token = await PayPalInvoices.getToken(config.clientID, config.secretID);
-	PayPalInvoices.getInvoiceInfo(token, "INV2-SQWP-6UB6-8JEM-EXF9");
-}
-//https: //www.sandbox.paypal.com/invoice/p/#INV2UDXGSFY2Q46RAVXL
-init();
